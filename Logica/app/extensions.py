0c05ee9bf673c0cfa3dfdd97061fd20ca415a82c
RyogaAqua <emmanuelarguelles48@gmail.com>
2025-05-19 22:14:29 -0400

    Agregue el README

--- Logica/app/extensions.py
+++ Logica/app/extensions.py
@@ -1,7 +1,7 @@
 # filepath: c:\Users\emman\Documents\Restaurante\Logica\app\extensions.py
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from flask_cors import CORS  # Asegúrate de que flask-cors esté instalado
+from flask_cors import CORS 
 from flask_wtf import CSRFProtect
 
 # Instancia de SQLAlchemy para manejar la base de datos
